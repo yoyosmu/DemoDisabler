b03f4d23128b4ccb7e2e239f148fc13341a794b5
Yoyosmu <jun.mu57@gmail.com>
2026-05-12 19:54:10 -0400

    Update DemoDisabler.java

--- src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
+++ src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
@@ -2,12 +2,12 @@ package com.github.dedinc.demodisabler;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,6 +18,7 @@ public class DemoDisabler {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final Minecraft mc = Minecraft.getInstance();
     private static boolean disabled = false;
+    
     private static final ResourceLocation DEMO_OVERLAY = new ResourceLocation("minecraft", "demo");
 
     public DemoDisabler() {
@@ -27,7 +28,7 @@ public class DemoDisabler {
     @SubscribeEvent
     public void onRenderOverlay(RenderGuiOverlayEvent.Pre event) {
         if (DEMO_OVERLAY.equals(event.getOverlay().id())) {
-            event.setCanceled(true);
+            event.setCanceled(true); 
         }
     }
 
@@ -41,21 +42,22 @@ public class DemoDisabler {
 
     private void disableDemoMode() {
         try {
-            setPrivateField(Minecraft.class, mc, "demo", false);
-            setPrivateField(Minecraft.class, mc, "allowsMultiplayer", true);
-            setPrivateField(Minecraft.class, mc, "allowsChat", true);
+            setPrivateField(Minecraft.class, mc, "f_91040_", false);           
+            setPrivateField(Minecraft.class, mc, "f_91045_", true);           
+            setPrivateField(Minecraft.class, mc, "f_91046_", true);           
 
-            Object service = getPrivateField(Minecraft.class, mc, "socialInteractionsService");
+            Object service = getPrivateField(Minecraft.class, mc, "f_91041_");
             if (service != null) {
                 setPrivateField(service.getClass(), service, "serversAllowed", true);
                 setPrivateField(service.getClass(), service, "chatAllowed", true);
             }
 
-            LOGGER.info("Demo mode disabled for 1.20.1 via Reflection.");
+            LOGGER.info("Demo mode bypassed for 1.20.1 using legacy Reflection.");
             disabled = true;
+            
             mc.setScreen(new TitleScreen());
         } catch (Exception e) {
-            LOGGER.error("Failed to disable demo mode: ", e);
+            LOGGER.error("CRITICAL: Failed to find obfuscated fields in 1.20.1: ", e);
         }
     }
 
