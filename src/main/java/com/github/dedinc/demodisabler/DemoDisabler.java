b3f4a26a69e90b950b1911a05d5510e6bef53105
Yoyosmu <jun.mu57@gmail.com>
2026-05-12 19:23:22 -0400

    Update DemoDisabler.java

--- src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
+++ src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
@@ -2,10 +2,12 @@ package com.github.dedinc.demodisabler;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,9 +18,17 @@ public class DemoDisabler {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final Minecraft mc = Minecraft.getInstance();
     private static boolean disabled = false;
+    private static final ResourceLocation DEMO_OVERLAY = new ResourceLocation("minecraft", "demo");
 
     public DemoDisabler() {
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onRenderOverlay(RenderGuiOverlayEvent.Pre event) {
+        if (DEMO_OVERLAY.equals(event.getOverlay().id())) {
+            event.setCanceled(true);
+        }
     }
 
     @SubscribeEvent
@@ -35,17 +45,17 @@ public class DemoDisabler {
             setPrivateField(Minecraft.class, mc, "allowsMultiplayer", true);
             setPrivateField(Minecraft.class, mc, "allowsChat", true);
 
-            Object service = getPrivateField(Minecraft.class, mc, "socialInteractions");
+            Object service = getPrivateField(Minecraft.class, mc, "socialInteractionsService");
             if (service != null) {
                 setPrivateField(service.getClass(), service, "serversAllowed", true);
                 setPrivateField(service.getClass(), service, "chatAllowed", true);
             }
 
-            LOGGER.info("Demo mode disabled using Reflection.");
+            LOGGER.info("Demo mode disabled for 1.20.1 via Reflection.");
             disabled = true;
             mc.setScreen(new TitleScreen());
         } catch (Exception e) {
-            LOGGER.error("Failed to disable demo mode via reflection: ", e);
+            LOGGER.error("Failed to disable demo mode: ", e);
         }
     }
 
