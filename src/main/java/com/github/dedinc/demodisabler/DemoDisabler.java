610a255a7cfd251d4d48c804162d633974256fff
Yoyosmu <jun.mu57@gmail.com>
2026-05-12 16:18:17 -0400

    Update DemoDisabler.java

--- src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
+++ src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
@@ -1,16 +1,17 @@
 package com.github.dedinc.demodisabler;
 
-import com.mojang.authlib.yggdrasil.YggdrasilSocialInteractionsService;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.MainMenuScreen;
-import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.AddGuiOverlayLayersEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.lang.reflect.Field;
 
 @Mod("demodisablermod")
 public class DemoDisabler {
@@ -19,50 +20,40 @@ public class DemoDisabler {
     private static boolean disabled = false;
 
     public DemoDisabler() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
+        modBus.addListener(this::onAddLayers);
+    }
+
+    public void onAddLayers(AddGuiOverlayLayersEvent event) {
+        // Modern API to hide the "minecraft:demo" overlay [4, 5]
+        event.addConditionTo(ResourceLocation.withDefaultNamespace("demo"), () -> !disabled);
     }
 
     @SubscribeEvent
-    public void onInitGuiPost(GuiScreenEvent.InitGuiEvent.Post event) {
+    public void onInitGuiPost(ScreenEvent.Init.Post event) {
         if (disabled) return;
-
-        if (event.getGui() instanceof MainMenuScreen) {
+        if (event.getScreen() instanceof TitleScreen) {
             disableDemoMode();
         }
     }
 
     private void disableDemoMode() {
         try {
-            Field demoField = Minecraft.class.getDeclaredField("field_71459_aj");
-            demoField.setAccessible(true);
+            // Field names must use SRG names for Access Transformers [6]
+            mc.demo = false;
+            mc.allowsMultiplayer = true;
+            mc.allowsChat = true;
 
-            Field allowsMultiplayerField = Minecraft.class.getDeclaredField("field_238175_ae_");
-            allowsMultiplayerField.setAccessible(true);
+            var service = mc.socialInteractionsService;
+            if (service != null) {
+                service.serversAllowed = true;
+                service.chatAllowed = true;
+            }
 
-            Field allowsChatField = Minecraft.class.getDeclaredField("field_238176_af_");
-            allowsChatField.setAccessible(true);
-
-            demoField.setBoolean(mc, false);
-            allowsMultiplayerField.setBoolean(mc, true);
-            allowsChatField.setBoolean(mc, true);
-
-            Field serviceField = Minecraft.class.getDeclaredField("field_244734_au");
-            serviceField.setAccessible(true);
-            YggdrasilSocialInteractionsService service = (YggdrasilSocialInteractionsService) serviceField.get(mc);
-
-            Field serversAllowedField = YggdrasilSocialInteractionsService.class.getDeclaredField("serversAllowed");
-            serversAllowedField.setAccessible(true);
-            serversAllowedField.setBoolean(service, true);
-
-            Field chatAllowedField = YggdrasilSocialInteractionsService.class.getDeclaredField("chatAllowed");
-            chatAllowedField.setAccessible(true);
-            chatAllowedField.setBoolean(service, true);
-
-            LOGGER.info("Demo mode has been disabled.");
+            LOGGER.info("Demo mode disabled.");
             disabled = true;
-
-            mc.popGuiLayer();
-            mc.setScreen(new MainMenuScreen());
+            mc.setScreen(new TitleScreen());
         } catch (Exception e) {
             LOGGER.error("Failed to disable demo mode: ", e);
         }
