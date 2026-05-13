f7302e9927a2c4a5c7564034982d5578939debdc
Yoyosmu <jun.mu57@gmail.com>
2026-05-12 20:03:27 -0400

    Update DemoDisabler.java

--- src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
+++ src/main/java/com/github/dedinc/demodisabler/DemoDisabler.java
@@ -1,9 +1,8 @@
 package com.github.dedinc.demodisabler;
 
+import com.mojang.authlib.yggdrasil.YggdrasilSocialInteractionsService;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,23 +17,16 @@ public class DemoDisabler {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final Minecraft mc = Minecraft.getInstance();
     private static boolean disabled = false;
-    
-    private static final ResourceLocation DEMO_OVERLAY = new ResourceLocation("minecraft", "demo");
 
     public DemoDisabler() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void onRenderOverlay(RenderGuiOverlayEvent.Pre event) {
-        if (DEMO_OVERLAY.equals(event.getOverlay().id())) {
-            event.setCanceled(true); 
-        }
-    }
-
-    @SubscribeEvent
     public void onInitGuiPost(ScreenEvent.Init.Post event) {
         if (disabled) return;
+
+        // In 1.20.1, MainMenuScreen is now TitleScreen
         if (event.getScreen() instanceof TitleScreen) {
             disableDemoMode();
         }
@@ -42,22 +34,25 @@ public class DemoDisabler {
 
     private void disableDemoMode() {
         try {
-            setPrivateField(Minecraft.class, mc, "f_91040_", false);           
-            setPrivateField(Minecraft.class, mc, "f_91045_", true);           
-            setPrivateField(Minecraft.class, mc, "f_91046_", true);           
+            setPrivateField(Minecraft.class, mc, "f_91040_", false);
+            setPrivateField(Minecraft.class, mc, "f_91045_", true);
+            setPrivateField(Minecraft.class, mc, "f_91046_", true);
 
-            Object service = getPrivateField(Minecraft.class, mc, "f_91041_");
+            Field serviceField = Minecraft.class.getDeclaredField("f_91041_");
+            serviceField.setAccessible(true);
+            YggdrasilSocialInteractionsService service = (YggdrasilSocialInteractionsService) serviceField.get(mc);
+
             if (service != null) {
-                setPrivateField(service.getClass(), service, "serversAllowed", true);
-                setPrivateField(service.getClass(), service, "chatAllowed", true);
+                setPrivateField(YggdrasilSocialInteractionsService.class, service, "serversAllowed", true);
+                setPrivateField(YggdrasilSocialInteractionsService.class, service, "chatAllowed", true);
             }
 
-            LOGGER.info("Demo mode bypassed for 1.20.1 using legacy Reflection.");
+            LOGGER.info("Demo mode has been disabled via 1.20.1 Reflection.");
             disabled = true;
-            
+
             mc.setScreen(new TitleScreen());
         } catch (Exception e) {
-            LOGGER.error("CRITICAL: Failed to find obfuscated fields in 1.20.1: ", e);
+            LOGGER.error("Failed to disable demo mode: ", e);
         }
     }
 
@@ -65,11 +60,5 @@ public class DemoDisabler {
         Field field = clazz.getDeclaredField(fieldName);
         field.setAccessible(true);
         field.set(instance, value);
-    }
-
-    private Object getPrivateField(Class<?> clazz, Object instance, String fieldName) throws Exception {
-        Field field = clazz.getDeclaredField(fieldName);
-        field.setAccessible(true);
-        return field.get(instance);
     }
 }
